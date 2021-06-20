8ccfde7bf9cb3f10f45667e9073c1bcbc3dc5d0b
chr1s78 <ayahirano2014@126.com>
2021-06-20 13:47:27 +0800

    Initial Commit

---
+++ myGitHub/myGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  myGitHubApp.swift
+//  myGitHub
+//
+//  Created by 吕博 on 2021/6/20.
+//
+
+import SwiftUI
+
+@main
+struct myGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
